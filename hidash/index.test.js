a8fb94bb7463aa6bf66ccba2d0b690689f58e235
Ali Parandeh <Ali.Parandeh@outlook.com>
2019-12-28 22:06:47 +0000

    Fix 3 issues - global scope variables, running all tests, test descriptions added

--- hidash/index.test.js
+++ hidash/index.test.js
@@ -1,24 +1,35 @@
 const { forEach, map } = require("./index");
 
-let sum = 0;
-forEach([1, 2, 3], value => {
-  sum += value;
+const test = (desc, fn) => {
+  console.log("----", desc);
+  try {
+    fn();
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+test("The forEach function test", () => {
+  let sum = 0;
+  forEach([1, 2, 3], value => {
+    sum += value;
+  });
+
+  if (sum !== 7) {
+    throw new Error("Expected the summing array to equal 6");
+  }
 });
 
-if (sum !== 6) {
-  throw new Error("Expected the summing array to equal 6");
-}
+test("The map function test", () => {
+  const result = map([1, 2, 3], value => value * 2);
 
-const result = map([1, 2, 3], value => value * 2);
-
-if (result[0] != 2) {
-  throw new Error(`Expected to see 2 but saw ${result[0]}`);
-}
-if (result[1] != 4) {
-  throw new Error(`Expected to see 4 but saw ${result[1]}`);
-}
-if (result[2] != 6) {
-  throw new Error(`Expected to see 6 but saw ${result[2]}`);
-}
-
-console.log("Tests passed!!!");
+  if (result[0] != 2) {
+    throw new Error(`Expected to see 2 but saw ${result[0]}`);
+  }
+  if (result[1] != 4) {
+    throw new Error(`Expected to see 4 but saw ${result[1]}`);
+  }
+  if (result[2] != 6) {
+    throw new Error(`Expected to see 6 but saw ${result[2]}`);
+  }
+});
