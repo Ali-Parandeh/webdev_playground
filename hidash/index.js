7e9e21a95ccd56f2d44bb022a91b15101d2f30b2
Ali Parandeh <Ali.Parandeh@outlook.com>
2019-12-28 17:38:03 +0000

    Add for in implementation of forEach

--- hidash/index.js
+++ hidash/index.js
@@ -1,8 +1,15 @@
 module.exports = {
   forEach(arr, fn) {
-    for (let i = 0; i < arr.length; i++) {
-      const value = arr[i];
-      fn(value, i);
+    // Traditional Implementation
+    // for (let i = 0; i < arr.length; i++) {
+    //   const value = arr[i];
+    //   fn(value, i);
+    // }
+
+    // For in Implementation
+    // NOTE: When using for loops, in and of mean different things: For value of arr | for index in arr
+    for (let index in arr) {
+      fn(arr[index], index);
     }
   }
 };
