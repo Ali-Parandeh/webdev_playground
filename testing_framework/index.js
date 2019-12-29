fcf3d163da0b8c2e41729fdb341c587522bbd9c7
Ali Parandeh <Ali.Parandeh@outlook.com>
2019-12-29 14:34:16 +0000

    Add test file collector to the testing framework

--- testing_framework/index.js
+++ testing_framework/index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
+const Runner = require("./runner");
+const runner = new Runner();
 
-console.log("Running test!!.....");
+const run = async () => {
+  await runner.collectFiles(process.cwd());
+  console.log(runner.testFiles);
+  // testFiles === [{ name: 'Users/..../index.test.js'},
+  //                { name: 'Users/...../autocomplete.test.js'}]
+};
+
+run();
---
+++ testing_framework/runner.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+
+class Runner {
+  constructor() {
+    this.testFiles = [];
+  }
+
+  // Loop through the targetPath file directory tree and find all test files.
+  async collectFiles(targetPath) {
+    const files = await fs.promises.readdir(targetPath);
+    // files === ['index.html', 'test', 'utils.js', 'index.js', ...]
+
+    for (let file of files) {
+      const filepath = path.join(targetPath, file);
+      // filepath === '/Users/.../autocomplete.js'
+      const stats = await fs.promises.lstat(filepath);
+      //  stats = Stats {dev: 243242, mode: 4232, ....}
+
+      if (stats.isFile() && file.includes(".test.js")) {
+        this.testFiles.push({ name: filepath });
+        // testFiles === [{name: '/Users/.../autcomplete.test.js'}]
+      } else if (stats.isDirectory()) {
+        const childFiles = await fs.promises.readdir(filepath);
+        // childFiles === ['autocomplete.test.js', 'test.html']
+
+        files.push(...childFiles.map(f => path.join(file, f)));
+        // files === ['index.js', 'utils.js', 'test/autocomplete.test.js', 'test/test.html']
+
+        // NOTE: ... means expand the childFiles array and then push the items to the files array
+        // files.push(childFiles) === ['index.js', 'utils.js', ['autocomplete.test.js', 'test.html']]\
+        // files.push(...childFiles) === ['index.js', 'utils.js', 'autocomplete.test.js', 'test.html']
+      }
+    }
+  }
+}
+
+module.exports = Runner;
